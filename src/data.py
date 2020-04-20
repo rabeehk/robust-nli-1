fixed the bug in InferSent encoder, fixed the issue with extra average over the batch losses, added scripts to reproduce the transfer performance results

src/data.py
@@ -1,131 +1,89 @@
 import numpy as np
 import torch
 import pdb
+import os
 
-def get_duplicate_hypths(srcs):
-  hyps_counts = {}
-  for line in srcs:
-    hyp = line.split("|||")[-1].strip()
-    if hyp not in hyps_count:
-      hyps_count[hyp] = 0
-    hyps_count[hyp] += 1
+def get_batch(batch, word_vec, emb_dim=300):
+    # sent in batch in decreasing order of lengths (bsize, max_len, word_dim)
+    lengths = np.array([len(x) for x in batch])
+    max_len = np.max(lengths)
+    embed = np.zeros((max_len, len(batch), emb_dim))
 
-  hyps_dup = set()
-  for key,item in hyps_count.items():
-    if item > 1:
-      hyps_dup.add(item)
-  return hyps_dup
+    for i in range(len(batch)):
+        for j in range(len(batch[i])):
+            embed[j, i, :] = word_vec[batch[i][j]]
 
-def extract_from_file(lbls_file, srcs_file, max_sents, data_split, remove_dup):
-  labels_to_int = None
-  if "mpe" in lbls_file or "snli" in lbls_file or "multinli" in lbls_file or "sick" in lbls_file or "joci" in lbls_file or "glue" in lbls_file:
-    labels_to_int = {'entailment': 0, 'neutral': 1, 'contradiction': 2}
-  elif "spr" in lbls_file or "dpr" in lbls_file or "fnplus" in lbls_file or "add_one" in lbls_file:
-    labels_to_int = {'entailed': 0, 'not-entailed': 1}
-  elif "scitail" in lbls_file:
-    labels_to_int = {'entailment': 0, 'neutral': 1}
-  else:
-    print "Invalid lbls_file: %s" % (lbls_file)
+    return torch.from_numpy(embed).float(), lengths
 
-  data = {'lbls': [], 'hypoths': [], 'premises': []}
 
-  lbls = open(lbls_file).readlines()
-  srcs = open(srcs_file).readlines()
+def get_word_dict(sentences):
+    # create vocab of words
+    word_dict = {}
+    for sent in sentences:
+        for word in sent.split():
+            if word not in word_dict:
+                word_dict[word] = ''
+    word_dict['<s>'] = ''
+    word_dict['</s>'] = ''
+    word_dict['<p>'] = ''
+    return word_dict
 
-  '''
-  hyps_set = set()
-  if remove_dup:
-    hyps_set = get_duplicate_hypths(srcs)
-  '''
-  used_hyps = set()
 
-  assert len(lbls) == len(srcs), "%s: %s labels and source files are not same length" % (lbls_file, data_split)
-  num_duplicate_hyps = 0
+def get_glove(word_dict, glove_path):
+    # create word_vec with glove vectors
+    word_vec = {}
+    with open(glove_path) as f:
+        for line in f:
+            word, vec = line.split(' ', 1)
+            if word in word_dict:
+                word_vec[word] = np.array(list(map(float, vec.split())))
+    print('Found {0}(/{1}) words with glove vectors'.format(
+                len(word_vec), len(word_dict)))
+    return word_vec
 
-  added_sents = 0
-  for i in range(len(lbls)):
-    lbl = lbls[i].strip()
-    text  = srcs[i].split("|||")
-    hypoth = text[-1].strip()
-    premise = text[0].strip()
 
-    if remove_dup:
-      if hypoth in used_hyps: 
-        num_duplicate_hyps += 1
-        continue
-
-    if lbl not in labels_to_int:
-      print "bad label: %s" % (lbl)
-      continue
-
-    if added_sents >= max_sents:
-      continue
-
-    data['lbls'].append(labels_to_int[lbl])
-    data['hypoths'].append(hypoth)  
-    data['premises'].append(premise)
-    added_sents += 1
-
-    if remove_dup:
-      used_hyps.add(hypoth)
-
-  if remove_dup:
-    print "Removed %d duplicate hypotheses out of %d total" % (num_duplicate_hyps, len(lbls))
-
-  return data
-
-def get_nli_text(train_lbls_file, train_src_file, val_lbls_file, val_src_file, \
-                   test_lbls_file, test_src_file, max_train_sents, max_val_sents, max_test_sents, remove_dup=False):
-  labels = {}
-  hypoths = {}
-
-  train = extract_from_file(train_lbls_file, train_src_file, max_train_sents, "train", remove_dup)
-  val = extract_from_file(val_lbls_file, val_src_file, max_val_sents, "val", remove_dup)
-  test = extract_from_file(test_lbls_file, test_src_file, max_test_sents, "test", remove_dup)
-
-  return train, val, test
-
-def get_vocab(txt):
-  vocab = set()
-  for sent in txt:
-    for word in sent.split():
-      vocab.add(word)
-  return vocab
-
-def get_word_vecs(vocab, embdsfile, lorelei_embds=False):
-  word_vecs = {}
-  with open(embdsfile) as f:
-    for line in f:
-      word, vec = line.split(' ', 1)
-      if lorelei_embds:
-        word = word[4:]
-      if word in vocab:
-        word_vecs[word] = np.array(list(map(float, vec.split())))
-  print('Found {0}(/{1}) words with vectors'.format(
-            len(word_vecs), len(vocab)))
-  return word_vecs
+def build_vocab(sentences, glove_path):
+    word_dict = get_word_dict(sentences)
+    word_vec = get_glove(word_dict, glove_path)
+    print('Vocab size : {0}'.format(len(word_vec)))
+    return word_vec
 
 
 
-def build_vocab(txt, embdsfile, lorelei_embds=False):
-  vocab = get_vocab(txt)
-  vocab.add("OOV")
-  word_vecs = get_word_vecs(vocab, embdsfile, lorelei_embds)
-  print('Vocab size : {0}'.format(len(word_vecs)))
-  return word_vecs 
+def get_nli(data_path, n_classes):
+    s1 = {}
+    s2 = {}
+    target = {}
 
-def get_batch(batch, word_vec):
-  # sent in batch in decreasing order of lengths (bsize, max_len, word_dim)
-  lengths = np.array([len(x.split()) for x in batch])
-  max_len = np.max(lengths)
-  embed = np.zeros((max_len, len(batch), len(word_vec[word_vec.keys()[0]])))
+    if n_classes == 3:
+        dico_label = {'entailment': 0, 'neutral': 1, 'contradiction': 2, 'hidden': 0}
+    else:
+        dico_label = {'entailment': 0, 'neutral': 1, 'contradiction': 1, 'hidden': 0}
 
-  for i in range(len(batch)):
-    sent = batch[i].split()
-    for j in range(len(batch[i].split())):
-      if sent[j] not in word_vec:
-        embed[j, i, :] = word_vec["OOV"]
-      else:
-        embed[j, i, :] = word_vec[sent[j]]
+    for data_type in ['train', 'dev', 'test']:
+        s1[data_type], s2[data_type], target[data_type] = {}, {}, {}
+        s1[data_type]['path'] = os.path.join(data_path, 's1.' + data_type)
+        s2[data_type]['path'] = os.path.join(data_path, 's2.' + data_type)
+        target[data_type]['path'] = os.path.join(data_path,
+                                                 'labels.' + data_type)
 
-  return torch.from_numpy(embed).float(), lengths
+        s1[data_type]['sent'] = [line.rstrip() for line in
+                                 open(s1[data_type]['path'], 'r')]
+        s2[data_type]['sent'] = [line.rstrip() for line in
+                                 open(s2[data_type]['path'], 'r')]
+        target[data_type]['data'] = np.array([dico_label[line.rstrip('\n')]
+                                              for line in open(target[data_type]['path'], 'r')])
+
+        assert len(s1[data_type]['sent']) == len(s2[data_type]['sent']) == \
+               len(target[data_type]['data'])
+
+        print('** {0} DATA : Found {1} pairs of {2} sentences.'.format(
+            data_type.upper(), len(s1[data_type]['sent']), data_type))
+
+    train = {'s1': s1['train']['sent'], 's2': s2['train']['sent'],
+             'label': target['train']['data']}
+    dev = {'s1': s1['dev']['sent'], 's2': s2['dev']['sent'],
+           'label': target['dev']['data']}
+    test = {'s1': s1['test']['sent'], 's2': s2['test']['sent'],
+            'label': target['test']['data']}
+    return train, dev, test

src/eval.py
@@ -1,163 +1,155 @@
-import os
-import sys
-import time
 import argparse
+import sys
+from os.path import join
 
 import numpy as np
-
 import torch
 from torch.autograd import Variable
-import torch.nn as nn
 
-from data import get_nli_text, build_vocab, get_batch
-from models import SharedNLINet, SharedHypothNet, BLSTMEncoder
-from mutils import get_optimizer
+from mutils import write_to_csv
+from data import get_batch, build_vocab, get_nli
 
-IDX2LBL = {}
 
 def get_args():
-  parser = argparse.ArgumentParser(description='Training NLI model based on just hypothesis sentence')
+    parser = argparse.ArgumentParser(description='Training NLI model based on just hypothesis sentence')
 
-  # paths
-  parser.add_argument("--embdfile", type=str, default='../data/embds/glove.840B.300d.txt', help="File containin the word embeddings")
-  parser.add_argument("--outputdir", type=str, default='savedir/', help="Output directory")
-  parser.add_argument("--model", type=str, help="Input model that has already been trained")
-  parser.add_argument("--pred_file", type=str, default='preds', help="Suffix for the prediction files")
-  parser.add_argument("--train_lbls_file", type=str, default='../data/snli_1.0/cl_snli_train_lbl_file', help="NLI train data labels file (SNLI or MultiNLI)")
-  parser.add_argument("--train_src_file", type=str, default='../data/snli_1.0/cl_snli_train_source_file', help="NLI train data source file (SNLI or MultiNLI)")
-  parser.add_argument("--val_lbls_file", type=str, default='../data/snli_1.0/cl_snli_val_lbl_file', help="NLI validation (dev) data labels file (SNLI or MultiNLI)")
-  parser.add_argument("--val_src_file", type=str, default='../data/snli_1.0/cl_snli_val_source_file', help="NLI validation (dev) data source file (SNLI or MultiNLI)")
-  parser.add_argument("--test_lbls_file", type=str, default='../data/snli_1.0/cl_snli_test_lbl_file', help="NLI test data labels file (SNLI or MultiNLI)")
-  parser.add_argument("--test_src_file", type=str, default='../data/snli_1.0/cl_snli_test_source_file', help="NLI test data source file (SNLI or MultiNLI)")
+    # paths
+    parser.add_argument("--outputfile", type=str, default="results.csv", help="writes the results on this file.")
+    parser.add_argument("--word_emb_dim", type=int, default=300)
+    parser.add_argument("--embdfile", type=str, default='../data/embds/glove.840B.300d.txt',
+                        help="File containin the word embeddings")
+    parser.add_argument("--outputdir", type=str, default='savedir/', help="Output directory")
+    parser.add_argument("--model", type=str, help="Input model that has already been trained")
+    parser.add_argument("--pred_file", type=str, default='preds', help="Suffix for the prediction files")
+    parser.add_argument("--test_path", type=str, required=True, help="The path of the test dataset.")
+    parser.add_argument("--train_path", type=str, required=True, help="The path of the train dataset the model is trained on.")
+    parser.add_argument("--test_data", type=str, required=True, help="The name of the test dataset", choices=['SNLI',
+                       'SNLIHard', 'MNLIMatched', 'MNLIMismatched', 'JOCI', 'SICK-E', 'AddOneRTE', 'DPR',\
+                       'FNPLUS', 'SciTail','SPR','MPE', 'QQP', 'GLUEDiagnostic'])
+    parser.add_argument("--train_data", type=str, required=True, help="The name of the train dataset",  choices=['SNLI',
+                       'SNLIHard', 'MNLIMatched', 'MNLIMismatched', 'JOCI', 'SICK-E', 'AddOneRTE', 'DPR',\
+                       'FNPLUS', 'SciTail','SPR','MPE', 'QQP', 'GLUEDiagnostic'])
+
+    # data
+    parser.add_argument("--max_train_sents", type=int, default=10000000, help="Maximum number of training examples")
+    parser.add_argument("--max_val_sents", type=int, default=10000000, help="Maximum number of validation/dev examples")
+    parser.add_argument("--max_test_sents", type=int, default=10000000, help="Maximum number of test examples")
+    parser.add_argument("--lorelei_embds", type=int, default=0,
+                        help="Whether to use multilingual embeddings released for LORELEI. This requires cleaning up words since wordsare are prefixed with the language. 0 for no, 1 for yes (Default is 0)")
+
+    # model
+    parser.add_argument("--batch_size", type=int, default=64)
+
+    # gpu
+    parser.add_argument("--gpu_id", type=int, default=0, help="GPU ID")
+    parser.add_argument("--seed", type=int, default=1234, help="seed")
+
+    # misc
+    parser.add_argument("--verbose", type=int, default=1, help="Verbose output")
+
+    params, _ = parser.parse_known_args()
+
+    # print parameters passed, and all parameters
+    print('\ntogrep : {0}\n'.format(sys.argv[1:]))
+    print(params)
+
+    return params
 
 
-  # data
-  parser.add_argument("--max_train_sents", type=int, default=10000000, help="Maximum number of training examples")
-  parser.add_argument("--max_val_sents", type=int, default=10000000, help="Maximum number of validation/dev examples")
-  parser.add_argument("--max_test_sents", type=int, default=10000000, help="Maximum number of test examples")
-  parser.add_argument("--lorelei_embds", type=int, default=0, help="Whether to use multilingual embeddings released for LORELEI. This requires cleaning up words since wordsare are prefixed with the language. 0 for no, 1 for yes (Default is 0)")
-
-  # model
-  parser.add_argument("--encoder_type", type=str, default='BLSTMEncoder', help="see list of encoders")
-  parser.add_argument("--enc_lstm_dim", type=int, default=2048, help="encoder nhid dimension")
-  parser.add_argument("--n_enc_layers", type=int, default=1, help="encoder num layers")
-  parser.add_argument("--fc_dim", type=int, default=512, help="nhid of fc layers")
-  parser.add_argument("--n_classes", type=int, default=3, help="entailment/neutral/contradiction")
-  parser.add_argument("--pool_type", type=str, default='max', help="max or mean")
-  parser.add_argument("--batch_size", type=int, default=64)
-
-  # gpu
-  parser.add_argument("--gpu_id", type=int, default=-1, help="GPU ID")
-  parser.add_argument("--seed", type=int, default=1234, help="seed")
+def compute_score_with_logits(logits, labels, n_classes):
+    pred = logits.data.max(1)[1]
+    if n_classes == 2:
+        pred[pred == 2] = 1
+    correct = pred.long().eq(labels.data.long()).cpu().sum()
+    return correct
 
 
-  #misc
-  parser.add_argument("--verbose", type=int, default=1, help="Verbose output")
-
-  params, _ = parser.parse_known_args()
-
-  # print parameters passed, and all parameters
-  print('\ntogrep : {0}\n'.format(sys.argv[1:]))
-  print(params)
-
-  return params
-
-def evaluate(epoch, valid, params, word_vec, shared_nli_net, eval_type, pred_file):
-  shared_nli_net.eval()
-  correct = 0.
-  global val_acc_best, lr, stop_training, adam_stop
-
-  #if eval_type == 'valid':
-  print('\n{0} : Epoch {1}'.format(eval_type, epoch))
-
-  hypoths = valid['hypoths'] #if eval_type == 'valid' else test['s1']
-  premises = valid['premises'] #if eval_type == 'valid' else test['s2']
-  target = valid['lbls']
-
-  out_preds_f = open(pred_file, "wb")
-
-  for i in range(0, len(hypoths), params.batch_size):
-    # prepare batch
-    hypoths_batch, hypoths_len = get_batch(hypoths[i:i + params.batch_size], word_vec)
-    premises_batch, premises_len = get_batch(premises[i:i + params.batch_size], word_vec)
-    tgt_batch = None
-    if params.gpu_id > -1:
-      hypoths_batch = Variable(hypoths_batch.cuda())
-      premises_batch = Variable(premises_batch.cuda())
-      tgt_batch = Variable(torch.LongTensor(target[i:i + params.batch_size])).cuda()
+def get_nli_split(path, n_classes, split="test"):
+    data = {}
+    if n_classes == 3:
+        dico_labels = {'entailment': 0, 'neutral': 1, 'contradiction': 2, 'hidden': 3}
     else:
-      hypoths_batch = Variable(hypoths_batch)
-      premises_batch = Variable(premises_batch)
-      tgt_batch = Variable(torch.LongTensor(target[i:i + params.batch_size]))
+        dico_labels = {'entailment': 0, 'neutral': 1, 'contradiction': 1, 'hidden': 3}
+    data['s1'] = [line.rstrip() for line in open(join(path, 's1.' + split), 'r')]
+    data['s2'] = [line.rstrip() for line in open(join(path, 's2.' + split), 'r')]
+    data['labels'] = np.array([dico_labels[line.rstrip('\n')] for line in open(join(path, 'labels.' + split), 'r')])
+    return data
 
-    # model forward
-    output = shared_nli_net((premises_batch, premises_len), (hypoths_batch, hypoths_len))
 
-    all_preds = output.data.max(1)[1]
-    for pred in all_preds:
-      out_preds_f.write(IDX2LBL[pred.item()] + "\n")
-    correct += all_preds.long().eq(tgt_batch.data.long()).cpu().sum()
+def evaluate(args, nli_net, test_nlipath, n_classes, word_vec, split="test"):
+    test = get_nli_split(test_nlipath, n_classes, split)
+    for split in ['s1', 's2']:
+        test[split] = np.array([['<s>'] +
+                                [word for word in sent.split() if word in word_vec] +
+                                ['</s>'] for sent in test[split]])
 
-  out_preds_f.close()
-  # save model
-  eval_acc = round(100.0 * correct / len(hypoths), 2)
-  print('finalgrep : accuracy {0} : {1}'.format(eval_type, eval_acc))
+    # Evaluates on the test set.
+    correct = 0.
+    s1 = test['s1']
+    s2 = test['s2']
+    target = test['labels']
+    outputs = []
+    for i in range(0, len(s1), args.batch_size):
+        # prepare batch
+        s1_batch, s1_len = get_batch(s1[i:i + args.batch_size], word_vec, args.word_emb_dim)
 
-  return eval_acc
+        s2_batch, s2_len = get_batch(s2[i:i + args.batch_size], word_vec, args.word_emb_dim)
+
+        s1_batch, s2_batch = Variable(s1_batch.cuda()), Variable(s2_batch.cuda())
+        tgt_batch = Variable(torch.LongTensor(target[i:i + args.batch_size])).cuda()
+        output = nli_net((s1_batch, s1_len), (s2_batch, s2_len))
+        outputs.extend(output.data.max(1)[1].cpu().numpy())
+        correct += compute_score_with_logits(output, tgt_batch, n_classes)
+
+    eval_acc = round(100 * correct.item() / len(s1), 2)
+    print('evaluation accuracy is {0}'.format(eval_acc))
+    return eval_acc, outputs
+
+
+def get_vocab(args):
+    # build a vocabulary from all train,dev,test set of the actual snli plus the test set of the
+    # all the transfer tasks.
+    train, valid, test = {}, {}, {}
+    for split in ['test', 'valid', 'train']:
+        for s in ['s1', 's2']:
+            eval(split)[s] = []
+    for datapath, n_classes in [(args.test_path, args.data_to_n_classes[args.test_data]),
+                               (args.train_path, args.data_to_n_classes[args.train_data])]:
+        transfer_train, transfer_valid, transfer_test = get_nli(datapath, n_classes)
+        for split in ['test', 'valid', 'train']:
+            for s in ['s1', 's2']:
+                eval(split)[s].extend(eval("transfer_" + split)[s])
+
+    word_vec = build_vocab(train['s1'] + train['s2'] +
+                           valid['s1'] + valid['s2'] +
+                           test['s1'] + test['s2'], args.embdfile)
+    return word_vec
+
 
 def main(args):
-  print "main"
+    # sets seed.
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
 
-  """
-  SEED
-  """
-  np.random.seed(args.seed)
-  torch.manual_seed(args.seed)
-  torch.cuda.manual_seed(args.seed)
+    args.data_to_n_classes = {
+        'SNLI': 3, 'SNLIHard': 3, 'MNLIMatched': 3, 'MNLIMismatched': 3, 'JOCI': 3,\
+        'SICK-E': 3, 'AddOneRTE': 2, 'DPR': 2, 'FNPLUS': 2, 'SciTail': 2, 'SPR': 2,\
+        'MPE': 3, 'QQP': 2,'GLUEDiagnostic': 3
+    }
 
-  """
-  DATA
-  """
-  train, val, test = get_nli_text(args.train_lbls_file, args.train_src_file, args.val_lbls_file, \
-                                    args.val_src_file, args.test_lbls_file, args.test_src_file, \
-                                    args.max_train_sents, args.max_val_sents, args.max_test_sents)
+    # builds vocabulary from the all datasets.
+    word_vec = get_vocab(args)
+    shared_nli_net = torch.load(args.model).eval().cuda()
 
-  word_vecs = build_vocab(train['hypoths'] + val['hypoths'] + test['hypoths'] + train['premises'] + val['premises'] + test['premises'], \
-                          args.embdfile, args.lorelei_embds)
-  args.word_emb_dim = len(word_vecs[word_vecs.keys()[0]])
-
-  lbls_file = args.train_lbls_file
-  global IDX2LBL
-  if "mpe" in lbls_file or "snli" in lbls_file or "multinli" in lbls_file or "sick" in lbls_file or "joci" in lbls_file or "glue" in lbls_file:
-    IDX2LBL = {0: 'entailment', 1: 'neutral', 2: 'contradiction'}
-  elif "spr" in lbls_file or "dpr" in lbls_file or "fnplus" in lbls_file or "add_one" in lbls_file:
-    IDX2LBL = {0: 'entailed', 1: 'not-entailed', 2: 'not-entailed'}
-  elif "scitail" in lbls_file:
-    IDX2LBL = {0: 'entailment', 1: 'neutral', 2: 'neutral'}
-
-  shared_nli_net = torch.load(args.model)
-  print(shared_nli_net)
-
-  # loss
-  weight = torch.FloatTensor(args.n_classes).fill_(1)
-  loss_fn = nn.CrossEntropyLoss(weight=weight)
-  loss_fn.size_average = False
-
-  if args.gpu_id > -1:
-    shared_nli_net.cuda()
-    loss_fn.cuda()
-
-  """
-  Train model on Natural Language Inference task
-  """
-  epoch = 1
-
-  for pair in [(train, 'train'), (val, 'val'), (test, 'test')]:
-    #args.batch_size = len(pair[0]['lbls'])
-    eval_acc = evaluate(0, pair[0], args, word_vecs, shared_nli_net, pair[1], "%s/%s_%s" % (args.outputdir, pair[1], args.pred_file))
-    #epoch, valid, params, word_vec, nli_net, eval_type
+    eval_accs = {}
+    eval_accs["test"] = evaluate(args, shared_nli_net, args.test_path, \
+                                 args.data_to_n_classes[args.test_data], word_vec, split="test")[0]
+    eval_accs["dev"] = evaluate(args, shared_nli_net, args.test_path, \
+                                args.data_to_n_classes[args.test_data], word_vec, split="dev")[0]
+    write_to_csv(eval_accs, args, args.outputfile)
 
 
 if __name__ == '__main__':
-  args = get_args()
-  main(args)
+    args = get_args()
+    main(args)

src/train.py
@@ -2,7 +2,6 @@ import os
 import sys
 import time
 import argparse
-import pdb
 
 import numpy as np
 
@@ -10,32 +9,26 @@ import torch
 from torch.autograd import Variable
 import torch.nn as nn
 
-from data import get_nli_text, build_vocab, get_batch
-from models import SharedNLINet, SharedHypothNet, BLSTMEncoder
+from data import build_vocab, get_batch, get_nli
+from models import SharedNLINet, SharedHypothNet, InferSent
 from mutils import get_optimizer
 
 def get_args():
   parser = argparse.ArgumentParser(description='Training NLI model based on just hypothesis sentence')
 
   # paths
+  parser.add_argument("--use_early_stopping", action="store_true")
+  parser.add_argument("--version", type=int, default=2, help="Defines the version of the model.")
   parser.add_argument("--embdfile", type=str, default='../data/embds/glove.840B.300d.txt', help="File containin the word embeddings")
   parser.add_argument("--outputdir", type=str, default='savedir/', help="Output directory")
   parser.add_argument("--outputmodelname", type=str, default='model.pickle')
   parser.add_argument("--outputmodelname_adversary", type=str, default='model_adv.pickle')
-  parser.add_argument("--train_lbls_file", type=str, default='../data/snli_1.0/cl_snli_train_lbl_file', help="NLI train data labels file")
-  parser.add_argument("--train_src_file", type=str, default='../data/snli_1.0/cl_snli_train_source_file', help="NLI train data source file")
-  parser.add_argument("--val_lbls_file", type=str, default='../data/snli_1.0/cl_snli_val_lbl_file', help="NLI validation (dev) data labels file")
-  parser.add_argument("--val_src_file", type=str, default='../data/snli_1.0/cl_snli_val_source_file', help="NLI validation (dev) data source file")
-  parser.add_argument("--test_lbls_file", type=str, default='../data/snli_1.0/cl_snli_test_lbl_file', help="NLI test data labels file")
-  parser.add_argument("--test_src_file", type=str, default='../data/snli_1.0/cl_snli_test_source_file', help="NLI test data source file")
+  parser.add_argument("--nlipath", type=str, required=True, help="specifies the folder of the dataset")
 
   # data
   parser.add_argument("--max_train_sents", type=int, default=10000000, help="Maximum number of training examples")
   parser.add_argument("--max_val_sents", type=int, default=10000000, help="Maximum number of validation/dev examples")
   parser.add_argument("--max_test_sents", type=int, default=10000000, help="Maximum number of test examples")
-  parser.add_argument("--remove_dup", type=int, default=0, help="Whether to remove hypotheses that are duplicates. 0 for no, 1 for yes (remove them).")
-  parser.add_argument("--lorelei_embds", type=int, default=0, help="Whether to use multilingual embeddings released for LORELEI. This requires cleaning up words since wordsare are prefixed with the language. 0 for no, 1 for yes (Default is 0)")
-
   # training
   parser.add_argument("--n_epochs", type=int, default=20)
   parser.add_argument("--batch_size", type=int, default=64)
@@ -53,17 +46,17 @@ def get_args():
   parser.add_argument("--random_premise_frac", type=float, default=0., help="fraction of random premises to use in NLI net")
 
   # model
-  parser.add_argument("--encoder_type", type=str, default='BLSTMEncoder', help="see list of encoders")
+  parser.add_argument("--encoder_type", type=str, default='InferSent', help="see list of encoders")
   parser.add_argument("--enc_lstm_dim", type=int, default=2048, help="encoder nhid dimension")
   parser.add_argument("--n_enc_layers", type=int, default=1, help="encoder num layers")
   parser.add_argument("--fc_dim", type=int, default=512, help="nhid of fc layers")
-  parser.add_argument("--n_classes", type=int, default=3, help="entailment/neutral/contradiction")
+  parser.add_argument("--n_classes", type=int, default=3, required=True, help="entailment/neutral/contradiction")
   parser.add_argument("--pool_type", type=str, default='max', help="max or mean")
   parser.add_argument("--pre_trained_model", type=str, default='', help="Path to pre-trained model")
   parser.add_argument("--pre_trained_adv_model", type=str, default='', help="Path to pre-trained hypothesis model")
 
   # gpu
-  parser.add_argument("--gpu_id", type=int, default=-1, help="GPU ID")
+  parser.add_argument("--gpu_id", type=int, default=0, help="GPU ID")
   parser.add_argument("--seed", type=int, default=1234, help="seed")
 
 
@@ -101,13 +94,11 @@ def get_model_configs(params, n_words):
     'adv_hyp_encoder_lambda': params.adv_hyp_encoder_lambda,
     'nli_net_adv_hyp_encoder_lambda': params.nli_net_adv_hyp_encoder_lambda,
     'random_premise_frac'   : params.random_premise_frac  ,
+    'version'        :  params.version        , 
   }
 
   # model
-  encoder_types = ['BLSTMEncoder']
-                 #, 'BLSTMprojEncoder', 'BGRUlastEncoder',
-                 #'InnerAttentionMILAEncoder', 'InnerAttentionYANGEncoder',
-                 #'InnerAttentionNAACLEncoder', 'ConvNetEncoder', 'LSTMEncoder']
+  encoder_types = ['InferSent']
   assert params.encoder_type in encoder_types, "encoder_type must be in " + \
                                              str(encoder_types)
   return config_nli_model
@@ -118,19 +109,16 @@ def trainepoch(epoch, train, optimizer_nli, optimizer_hypoth, params, word_vec, 
   shared_nli_net.train()
   shared_hypoth_net.train()
   all_costs_nli, all_costs_hypoth = [], []
-  logs = []
   words_count = 0
 
   last_time = time.time()
   correct_nli, correct_hypoth = 0., 0.
-  # shuffle the data
-  permutation = np.random.permutation(len(train['hypoths']))
 
-  hypoths, premises, target = [], [], [] 
-  for i in permutation:
-    hypoths.append(train['hypoths'][i])
-    premises.append(train['premises'][i])
-    target.append(train['lbls'][i])
+  # shuffle the data
+  permutation = np.random.permutation(len(train['s1']))
+  premises = train['s1'][permutation]
+  hypoths = train['s2'][permutation]
+  target = train['label'][permutation]
 
   optimizer_nli.param_groups[0]['lr'] = optimizer_nli.param_groups[0]['lr'] * params.decay if epoch>1\
       and 'sgd' in params.optimizer else optimizer_nli.param_groups[0]['lr']
@@ -167,13 +155,13 @@ def trainepoch(epoch, train, optimizer_nli, optimizer_hypoth, params, word_vec, 
     # model forward
     output_nli = shared_nli_net((premises_batch, premises_len), (hypoths_batch, hypoths_len), random_premise)
     pred_nli = output_nli.data.max(1)[1]
-    correct_nli += pred_nli.long().eq(tgt_batch.data.long()).cpu().sum()
+    correct_nli += pred_nli.long().eq(tgt_batch.data.long()).cpu().sum().item()
     assert len(pred_nli) == len(hypoths[stidx:stidx + params.batch_size])
     
     if adv_lambda > 0:
       output_hypoth = shared_hypoth_net((hypoths_batch, hypoths_len))
       pred_hypoth = output_hypoth.data.max(1)[1]
-      correct_hypoth += pred_hypoth.long().eq(tgt_batch.data.long()).cpu().sum()
+      correct_hypoth += pred_hypoth.long().eq(tgt_batch.data.long()).cpu().sum().item()
       assert len(pred_hypoth) == len(hypoths[stidx:stidx + params.batch_size])
 
     # loss
@@ -204,7 +192,7 @@ def trainepoch(epoch, train, optimizer_nli, optimizer_hypoth, params, word_vec, 
         if p.requires_grad:
           #p.grad.data.div_(k)  # divide by the actual batch size
           total_norm += p.grad.data.norm() ** 2
-      total_norm = np.sqrt(total_norm)
+      total_norm = np.sqrt(total_norm.item())
 
       if total_norm > params.max_norm:
           shrink_factor = params.max_norm / total_norm
@@ -221,16 +209,14 @@ def trainepoch(epoch, train, optimizer_nli, optimizer_hypoth, params, word_vec, 
     if adv_lambda > 0:
       clip_gradients_and_step(shared_hypoth_net, k, optimizer_hypoth)
 
-
     if len(all_costs_nli) == 100:
-      logs.append('{0} ; loss nli {1} ; loss hypoth {2} ; sentence/s {3} ; words/s {4} ; accuracy train nli {5} ; accuracy train hypoth {6}'.format(
+      print('{0} ; loss nli {1} ; loss hypoth {2} ; sentence/s {3} ; words/s {4} ; accuracy train nli {5} ; accuracy train hypoth {6}'.format(
                             stidx, round(np.mean(all_costs_nli), 2),
                             round(np.mean(all_costs_hypoth), 2),
                             int(len(all_costs_nli) * params.batch_size / (time.time() - last_time)),
                             int(words_count * 1.0 / (time.time() - last_time)),
                             round(100.*correct_nli/(stidx+k), 2), 
                             round(100.*correct_hypoth/(stidx+k),2)))
-      print(logs[-1])
       last_time = time.time()
       words_count = 0
       all_costs_nli, all_costs_hypoth = [], []
@@ -250,9 +236,9 @@ def evaluate(epoch, valid, optimizer_nli, optimizer_hypoth, params, word_vec, sh
   if eval_type == 'valid':
     print('\n{0} : Epoch {1}'.format(eval_type, epoch))
 
-  hypoths = valid['hypoths'] #if eval_type == 'valid' else test['s1']
-  premises = valid['premises'] #if eval_type == 'valid' else test['s2']
-  target = valid['lbls']
+  hypoths = valid['s2']
+  premises = valid['s1']
+  target = valid['label']
 
   for i in range(0, len(hypoths), params.batch_size):
     # prepare batch
@@ -271,11 +257,11 @@ def evaluate(epoch, valid, optimizer_nli, optimizer_hypoth, params, word_vec, sh
     # model forward
     output_nli = shared_nli_net((premises_batch, premises_len), (hypoths_batch, hypoths_len))
     pred_nli = output_nli.data.max(1)[1]
-    correct_nli += pred_nli.long().eq(tgt_batch.data.long()).cpu().sum()
+    correct_nli += pred_nli.long().eq(tgt_batch.data.long()).cpu().sum().item()
     if adv_lambda > 0:
       output_hypoth = shared_hypoth_net((hypoths_batch, hypoths_len))
       pred_hypoth = output_hypoth.data.max(1)[1]
-      correct_hypoth += pred_hypoth.long().eq(tgt_batch.data.long()).cpu().sum()
+      correct_hypoth += pred_hypoth.long().eq(tgt_batch.data.long()).cpu().sum().item()
 
   # save model
   eval_acc_nli = round(100. * correct_nli / len(hypoths), 2)
@@ -301,16 +287,15 @@ def evaluate(epoch, valid, optimizer_nli, optimizer_hypoth, params, word_vec, sh
         optimizer_hypoth.param_groups[0]['lr'] = optimizer_hypoth.param_groups[0]['lr'] / params.lrshrink
         print('Shrinking hypoth lr by : {0}. New lr = {1}'.format(params.lrshrink,
                               optimizer_hypoth.param_groups[0]['lr']))
-        if optimizer_nli.param_groups[0]['lr'] < params.minlr:
+        if optimizer_nli.param_groups[0]['lr'] < params.minlr and params.use_early_stopping:
           stop_training = True
-      if 'adam' in params.optimizer:
+      if 'adam' in params.optimizer and params.use_early_stopping:
         # early stopping (at 2nd decrease in accuracy)
         stop_training = adam_stop
         adam_stop = True
   return eval_acc_nli, eval_acc_hypoth
 
 def main(args):
-  print "main"
 
   """
   SEED
@@ -323,20 +308,24 @@ def main(args):
   """
   DATA
   """
-  train, val, test = get_nli_text(args.train_lbls_file, args.train_src_file, args.val_lbls_file, \
-                                    args.val_src_file, args.test_lbls_file, args.test_src_file, \
-                                    args.max_train_sents, args.max_val_sents, args.max_test_sents, args.remove_dup)
+  train, valid, test = get_nli(args.nlipath, args.n_classes)
+  word_vecs = build_vocab(train['s1'] + train['s2'] +
+                       valid['s1'] + valid['s2'] +
+                       test['s1'] + test['s2'], args.embdfile)
 
-  word_vecs = build_vocab(train['hypoths'] + val['hypoths'] + test['hypoths'] + train['premises'] + val['premises'] + test['premises'] , args.embdfile, args.lorelei_embds)
-  args.word_emb_dim = len(word_vecs[word_vecs.keys()[0]])
+  for split in ['s1', 's2']:
+    for data_type in ['train', 'valid', 'test']:
+        eval(data_type)[split] = np.array([['<s>'] +
+            [word for word in sent.split() if word in word_vecs] +
+            ['</s>'] for sent in eval(data_type)[split]])
+
+
+  args.word_emb_dim = len(word_vecs[list(word_vecs.keys())[0]])
 
   nli_model_configs = get_model_configs(args, len(word_vecs))
 
-  lbls_file = args.train_lbls_file
-  if "mpe" in lbls_file or "snli" in lbls_file or "multinli" in lbls_file or "sick" in lbls_file or "joci" in lbls_file or "glue" in lbls_file:
-    nli_model_configs["n_classes"] = 3
-  elif "spr" in lbls_file or "dpr" in lbls_file or "fnplus" in lbls_file or "add_one" in lbls_file or "scitail" in lbls_file:
-    nli_model_configs["n_classes"] = 2
+
+  nli_model_configs["n_classes"] = args.n_classes
 
   # define premise and hypoth encoders
   premise_encoder = eval(nli_model_configs['encoder_type'])(nli_model_configs)
@@ -347,7 +336,7 @@ def main(args):
   print(shared_hypoth_net)
 
   if args.pre_trained_model:
-    print "Pre_trained_model: " + args.pre_trained_model
+    print( "Pre_trained_model: " + args.pre_trained_model)
     pre_trained_model = torch.load(args.pre_trained_model)
   
     shared_nli_net_params = shared_nli_net.state_dict()
@@ -361,7 +350,7 @@ def main(args):
   print(shared_nli_net)
 
   if args.pre_trained_adv_model:
-    print "Pre_trained_adv_model: " + args.pre_trained_adv_model
+    print( "Pre_trained_adv_model: " + args.pre_trained_adv_model)
     pre_trained_model = torch.load(args.pre_trained_adv_model)
   
     shared_hypoth_net_params = shared_hypoth_net.state_dict()
@@ -414,7 +403,7 @@ def main(args):
 
   while not stop_training and epoch <= args.n_epochs:
     train_acc_nli, train_acc_hypoth, shared_nli_net, shared_hypoth_net = trainepoch(epoch, train, optimizer_nli, optimizer_hypoth, args, word_vecs, shared_nli_net, shared_hypoth_net, loss_fn_nli, loss_fn_hypoth, args.adv_lambda, args.adv_hyp_encoder_lambda)
-    eval_acc_nli, eval_acc_hypoth = evaluate(epoch, val, optimizer_nli, optimizer_hypoth, args, word_vecs, shared_nli_net, shared_hypoth_net, 'valid', adv_lambda=args.adv_lambda)
+    eval_acc_nli, eval_acc_hypoth = evaluate(epoch, valid, optimizer_nli, optimizer_hypoth, args, word_vecs, shared_nli_net, shared_hypoth_net, 'valid', adv_lambda=args.adv_lambda)
     epoch += 1
 
 